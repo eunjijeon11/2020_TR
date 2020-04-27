a1fd866263262e2420829f5ceacf67c790936274
전또맹 <eunjiamy@naver.com>
2020-04-27 21:00:33 +0900

    frag1완성

--- app/src/main/java/com/example/mainscreen/Quiz_End.java
+++ app/src/main/java/com/example/mainscreen/Quiz_End.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class Quiz_End extends AppCompatActivity {
 
     int 점수;
     ListView ox;
     int[] list;
+    TextView score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +21,11 @@ public class Quiz_End extends AppCompatActivity {
         setContentView(R.layout.activity_quiz__end);
 
         ox = (ListView) findViewById(R.id.ox);
+        score = (TextView)findViewById(R.id.score);
 
         Intent intent = getIntent();
         점수 = intent.getExtras().getInt("점수");
+        score.setText(점수);
+
     }
 }
