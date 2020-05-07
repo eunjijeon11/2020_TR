ed78b07a762007d9e8a9c2440fd099b8145b15f1
전또맹 <eunjiamy@naver.com>
2020-05-07 17:32:15 +0900

    20200507은지

---
+++ app/src/main/java/com/example/mainscreen/Data.java
@@ -0,0 +1,26 @@
+package com.example.mainscreen;
+
+import android.graphics.Bitmap;
+
+public class Data {
+
+    private int oxId;
+    private Bitmap oxquizid;
+
+    public int getoxId() {
+        return oxId;
+    }
+
+    public void setoxId(int resId) {
+        this.oxId = resId;
+    }
+
+    public Bitmap getoxquizId() {
+        return oxquizid;
+    }
+
+    public void setOxquizId(Bitmap oxquizid) {
+        this.oxquizid = oxquizid;
+    }
+
+}
